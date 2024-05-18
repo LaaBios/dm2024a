948f804fe2d3fb641f751292ed86e58666b9b7d1
LaaBios <ayrtonvignolo@gmail.com>
2024-05-18 23:05:50 +0000

    auto commit 1       20240518 230549

--- src/lightgbm/451_lightgbm_binaria_BO.r
+++ src/lightgbm/451_lightgbm_binaria_BO.r
@@ -1,4 +1,4 @@
-# Este script esta pensado para correr en Google Cloud k 
+# Este script esta pensado para correr en Google Cloud k
 
 # se entrena con clase_binaria2  POS =  { BAJA+1, BAJA+2 }
 # Optimizacion Bayesiana de hiperparametros de  lightgbm,
@@ -43,7 +43,7 @@ PARAM$input$training <- c(202107) # los meses en los que vamos a entrenar
 PARAM$trainingstrategy$undersampling <- 1.0
 
 PARAM$hyperparametertuning$iteraciones <- 120
-PARAM$hyperparametertuning$xval_folds <- 5
+PARAM$hyperparametertuning$xval_folds <- 5          ##cantidad de folds para cross validation
 PARAM$hyperparametertuning$POS_ganancia <- 117000
 PARAM$hyperparametertuning$NEG_ganancia <- -3000
 
@@ -55,7 +55,10 @@ hs <- makeParamSet(
   makeNumericParam("feature_fraction", lower = 0.2, upper = 1.0),
   makeIntegerParam("min_data_in_leaf", lower = 1L, upper = 8000L),
   makeIntegerParam("num_leaves", lower = 16L, upper = 1024L),
-  makeIntegerParam("envios", lower = 5000L, upper = 15000L)
+  makeIntegerParam("envios", lower = 5000L, upper = 15000L),
+  makeNumericParam("lambda_l1", lower = 0.0, upper = 1.0),
+  makeNumericParam("lambda_l2", lower = 0.0, upper = 10.0),
+  makeNumericParam("min_gain_to_split", lower = 0.0, upper = 10.0)
 )
 
 #------------------------------------------------------------------------------
@@ -139,9 +142,9 @@ EstimarGanancia_lightgbm <- function(x) {
     feature_pre_filter = FALSE,
     verbosity = -100,
     max_depth = -1, # -1 significa no limitar,  por ahora lo dejo fijo
-    min_gain_to_split = 0.0, # por ahora, lo dejo fijo
-    lambda_l1 = 0.0, # por ahora, lo dejo fijo
-    lambda_l2 = 0.0, # por ahora, lo dejo fijo
+   # min_gain_to_split = 0.0, # por ahora, lo dejo fijo
+   # lambda_l1 = 0.0, # por ahora, lo dejo fijo
+   # lambda_l2 = 0.0, # por ahora, lo dejo fijo
     max_bin = 31, # por ahora, lo dejo fijo
     num_iterations = 9999, # valor grande, lo limita early_stopping_rounds
     force_row_wise = TRUE, # para evitar warning
