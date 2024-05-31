9c248d177f521de108553ae51d2093de4641fa13
LaaBios <ayrtonvignolo@gmail.com>
2024-05-31 23:15:49 +0000

    auto commit 1       20240531 231548

--- src/workflow-01/509_workflow_base.r
+++ src/workflow-01/509_workflow_base.r
@@ -257,20 +257,18 @@ TS_strategy_base9 <- function( pinputexps )
 
 
   param_local$future <- c(202109)
-  param_local$final_train <- c(202007, 202105, 202104,
-    202103, 202102, 202101, 202012, 202011, 202010, 202009,
-    202008, 202005, 202004, 202003, 202002, 202001, 201912, 201911) #esta linea la agregue yo y sin 202006
+  param_local$final_train <- c(202007, 202006, 202105, 202104,
+    202103, 202102, 202101, 202012, 202011, 202010, 202009) 
 
 
   param_local$train$training <- c(202105, 202104, 202103,
-     202102, 202101, 202012, 202011, 202010, 202009,
-     202008, 202007, 202005, 202004, 202003, 202002, 202001, 201912, 201911) #esta linea la agregue yo, sin 202006 y sumando nov y dic 2019
+     202102, 202101, 202012, 202011, 202010, 202009)
   param_local$train$validation <- c(202106)
   param_local$train$testing <- c(202107)
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling
-  param_local$train$undersampling <- 0.7 #cambio 0.2 por 0.7
+  param_local$train$undersampling <- 0.2
   param_local$train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
 
   return( exp_correr_script( param_local ) ) # linea fija
@@ -364,8 +362,8 @@ ZZ_final_base9 <- function( pinputexps )
   param_local$train$gan0 <-  -3000
   param_local$train$meseta <- 2001
 
-  param_local$kaggle$envios_desde <-  9500L #cambie, estaba 8000
-  param_local$kaggle$envios_hasta <- 13000L #cambie, estaba 14000
+  param_local$kaggle$envios_desde <-  8000
+  param_local$kaggle$envios_hasta <- 14000
   param_local$kaggle$envios_salto <-   500L
   param_local$kaggle$competition <- "itba-data-mining-2024-a"
 
